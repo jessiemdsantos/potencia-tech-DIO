7942e600ca04992a667cc9f970f6579d1e1f4051
jessiemdsantos <jessie.md.santos@gmail.com>
2023-05-28 02:02:11 -0300

    criando o content e pegando os dados de forma dinâmica

---
+++ Blog/angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,44 @@
+export const datafake = [
+    {
+        "id": "1",
+        "photo": "https://www.bigscal.com/wp-content/uploads/2023/04/Angular-vs-React-vs-Vue-Which-Framework-Is-Better-2023.png1_.png",
+        "title": "Angular vs React vs Vue: What’s popular in 2023?",
+        "description": `If you’re a new developer or you’re looking to add some new skills and frameworks into the mix in 2023, what should you focus on?
+        First, you want a tech stack that’ll help you add to your skills this year, and for years to come. While some frameworks are becoming less and less seen in production – borderline obsolete – so they aren’t the ones you’d want to start learning. 
+        `
+    },
+    {
+        "id": "2",
+        "photo": "https://www.revistaferramental.com.br/admin/images/artigos/industria-5-0-o-que-e-como-aplicar.jpg",
+        "title": "Industry 5.0: The Future of Manufacturing Has Arrived",
+        "description": `European industry is a key driver in the economic and societal transitions that we are currently undergoing.
+
+        In order to remain the engine of prosperity, industry must lead the digital and green transitions.
+        
+        This approach provides a vison of industry that aims beyond efficiency and productivity as the sole goals, and reinforces the role and the contribution of industry to society.
+        
+        It places the wellbeing of the worker at the centre of the production process and uses new technologies to provide prosperity beyond jobs and growth while respecting the production limits of the planet.
+        
+        It complements the existing "Industry 4.0" approach by specifically putting research and innovation at the service of the transition to a sustainable, human-centric and resilient European industry.`
+    },
+    
+    {
+        "id": "3",
+        "photo": "https://i.ytimg.com/vi/vxqBm6_0vyk/maxresdefault.jpg",
+        "title": "A First Look at PyScript: Python in the Web Browser",
+        "description": `PyScript is a Pythonic alternative to Scratch, JSFiddle, and other “easy to use” programming frameworks, with the goal of making the web a friendly, hackable place where anyone can author interesting and interactive applications.
+
+        (…)
+        
+        PyScript is a meta project that aims to combine multiple open technologies into a framework that allows users to create sophisticated browser applications with Python. It integrates seamlessly with the way the DOM works in the browser and allows users to add Python logic in a way that feels natural both to web and Python developers . 
+        `
+    },
+    {
+        "id": "4",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJP_3sNyl-Roj1iUelEycBX_ahc-9n2IV0Cp2qZmG-kUcPGik4qrViKP_zamw5imAPI1U&usqp=CAU",
+        "title": "Top 10 Artificial Intelligence Technologies in 2023",
+        "description": `1 Nаturаl Lаnguаge Generаtiоn. -  2. Sрeeсh Reсоgnitiоn. -  3. Mасhine Leаrning Рlаtfоrms. -  4. Virtuаl Аgents. - 5. Deсisiоn Mаnаgement. -  
+        6. АI Орtimized Hаrdwаre. - 7. Deeр Leаrning Рlаtfоrms. - 8. Rоbоtiс Рrосess Аutоmаtiоn. - 9. Biоmetriсs. - 10. Сyber Defense.`
+        
+    },
+];
